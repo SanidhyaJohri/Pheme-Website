0b9361440e4920484958ba9e7f97f0ad51d7112b
Pranavchiku <goswami.4@iitj.ac.in>
2022-06-15 17:10:38 +0530

    commit-index

--- home/views.py
+++ home/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.http import response, HttpResponse
+from .models import *
 # Create your views here.
 def index(request):
-    return render (request, "index.html")
+
+    welcome=WelcomeMessage.objects.all()[0]
+    welcomeHeader=welcome.welcomeHeader
+    welcomeParagraph=welcome.welcomeParagraph
+
+    context={
+        "welcomeHeader": welcomeHeader,
+        "welcomeParagraph": welcomeParagraph,
+    }
+    
+    return render (request, "index.html",context)
 
 def about(request):
     return render (request, "about.html")
