05bd06ab1fc70217a92a0fa3612b88cf5b59c2f4
Sanidhya S. Johri <johri.2@iitj.ac.in>
2022-06-15 16:56:16 +0530

    commit-about

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.http import response, HttpResponse
 # Create your views here.
 def index(request):
     return render (request, "index.html")
+
+def about(request):
+    return render (request, "about.html")
