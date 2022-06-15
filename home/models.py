18e14fea945a3acafb7fca5af848c4ae004a2c62
Sanidhya S. Johri <johri.2@iitj.ac.in>
2022-06-15 17:17:01 +0530

    commit-instagram

--- home/models.py
+++ home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class WelcomeMessage(models.Model):
+    welcomeHeader=models.CharField(max_length=1000)
+    welcomeParagraph=models.CharField(max_length=10000)
